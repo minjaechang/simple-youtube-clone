65a7a444ba03cfcb17781f3219603af9766ea932
Minjae Chang <minjaejchang@gmail.com>
2022-07-06 22:38:25 +1200

    Back to the list when searching

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,8 +1,13 @@
 import styles from './video_item.module.css';
 
-const VideoItem = ({ video: { snippet } }) => {
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === 'list' ? styles.list : styles.grid;
+
   return (
-    <li className={styles.container}>
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
       <div className={styles.video}>
         <img
           className={styles.thumbnail}
